d3544d2edd84a05c7fcd1267eef174401e2d14b7
Daniel Kim <danielkim1345@gmail.com>
2023-01-21 17:12:00 -0800

    working on speech

---
+++ src/RecordPage/RecordPage.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+const RecordPage = () => {
+    console.log("record page function")
+    const [data, setData] = useState(null);
+    function handleClick() {
+        fetch('http://localhost:5000/api/data')
+          .then(response => response.json())
+          .then(res => {setData(res)
+                console.log("response", res)
+                console.log("data", data)
+            });
+        console.log(data)
+    }
+
+  return (
+    <div>
+    <div>This is the Record Page</div>
+      <button onClick={handleClick}>Record now</button>
+      {data && <p>{data.message}</p>}
+    </div>
+  );
+};
+
+export default RecordPage;
