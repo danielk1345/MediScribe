0556c071dd136130a351d92f94993fe775beab05
m6keller <kellermatthewe@gmail.com>
2023-01-21 17:21:40 -0800

    cors enabled for localhost:3000

--- src/RecordPage/RecordPage.jsx
+++ src/RecordPage/RecordPage.jsx
@@ -2,21 +2,22 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 const RecordPage = () => {
-    console.log("record page function")
-    const [data, setData] = useState(null);
-    function handleClick() {
-        fetch('http://localhost:5000/api/data')
-          .then(response => response.json())
-          .then(res => {setData(res)
-                console.log("response", res)
-                console.log("data", data)
-            });
-        console.log(data)
-    }
+  console.log("record page function");
+  const [data, setData] = useState(null);
+  function handleClick() {
+    fetch("http://localhost:5000/speech-token-object")
+      .then((response) => response.json())
+      .then((res) => {
+        setData(res);
+        console.log("response", res);
+        console.log("data", data);
+      });
+    console.log(data);
+  }
 
   return (
     <div>
-    <div>This is the Record Page</div>
+      <div>This is the Record Page</div>
       <button onClick={handleClick}>Record now</button>
       {data && <p>{data.message}</p>}
     </div>
