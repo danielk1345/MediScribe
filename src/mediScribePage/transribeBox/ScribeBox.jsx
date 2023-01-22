2454b9e49e6feeb842118ac6dbaee7f957b91fe8
eh128 <ethanhsu128@gmail.com>
2023-01-22 08:47:21 -0800

    language

--- src/mediScribePage/transribeBox/ScribeBox.jsx
+++ src/mediScribePage/transribeBox/ScribeBox.jsx
@@ -1,10 +1,32 @@
 import { useEffect, useState } from "react";
 import Highlighter from "react-highlight-words";
 import PopUp from "./PopUp";
+import { REACT_APP_TRANSLATE_KEY } from "../../secrets";
 
-const ScribeBox = ({ recording, transcribing, matchingWords, input }) => {
+const ScribeBox = ({ recording, transcribing, matchingWords, input, lang }) => {
+  console.log(lang);
   const [word, setWord] = useState("");
   const [openPopup, setOpenPopup] = useState(false);
+  // const params = {
+  //   method: "POST",
+  //   headers: {
+  //     "Ocp-Apim-Subscription-Key": REACT_APP_TRANSLATE_KEY,
+  //     "Ocp-Apim-Subscription-Region": "eastus",
+  //     "Content-Type": "application/json",
+  //     charset: "UTF-8",
+  //   },
+  //   body: "[{'Text':'my name is Ethan and I am doing okay'}]",
+  // };
+  // const clicked = () => {
+  //   console.log("clicked");
+  //   fetch(
+  //     "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from=en&to=zh-Hans",
+  //     params
+  //   )
+  //     .then((response) => response.json())
+  //     .then((data) => console.log(data))
+  //     .catch((err) => console.log(err));
+  // };
 
   const CustomHighlight = (props) => {
     const togglePopup = (bool) => {
@@ -17,6 +39,7 @@ const ScribeBox = ({ recording, transcribing, matchingWords, input }) => {
             openPopup={openPopup}
             word={word}
             togglePopup={() => setOpenPopup(false)}
+            lang={lang}
           />
         ) : (
           ""
@@ -35,6 +58,7 @@ const ScribeBox = ({ recording, transcribing, matchingWords, input }) => {
   };
   return (
     <div className="text-container">
+      {/* <div onClick={clicked}>click me</div> */}
       <div className="scribe-text">
         {recording ? (
           "Generating transcript . . . "
--- src/mediScribePage/transribeBox/TranScribeBox.jsx
+++ src/mediScribePage/transribeBox/TranScribeBox.jsx
@@ -1,16 +1,40 @@
 import "./TranScribeBox.css";
 import ScribeBox from "./ScribeBox";
+import Select from "react-select";
+import { useState } from "react";
+
+const options = [
+  { value: "en", label: "English" },
+  { value: "fr", label: "French" },
+  { value: "zh-Han", label: "Chinese" },
+  { value: "es", label: "Spanish" },
+  { value: "ko", label: "Korean" },
+];
 
 const TranScribe = ({ recording, transcribing, matchingWords, input }) => {
+  const [selectedOption, setSelectedOption] = useState({
+    value: "en",
+    label: "English",
+  });
   return (
     <div className="transcribe-box">
       <div className="inner-box">
-        <div className="scribe-title">Transcript:</div>
+        <div className="top-row-cont">
+          <div className="scribe-title">Transcript:</div>
+          <div style={{ width: "30%" }}>
+            <Select
+              defaultValue={selectedOption}
+              onChange={setSelectedOption}
+              options={options}
+            />
+          </div>
+        </div>
         <ScribeBox
           recording={recording}
           transcribing={transcribing}
           matchingWords={matchingWords}
           input={input}
+          lang={selectedOption}
         />
       </div>
     </div>
