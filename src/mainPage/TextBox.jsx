8a243b3b59e0c1ab97eb1af20e3d46ce406d8b56
Daniel Kim <danielkim1345@gmail.com>
2023-01-22 01:20:55 -0800

    added firebase auth

--- src/mainPage/TextBox.jsx
+++ src/mainPage/TextBox.jsx
@@ -1,4 +1,30 @@
 import { Link } from "react-router-dom";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3v-9GvOgrGgyDQSZyCoTUatnwu-HLPm0",
+  authDomain: "mediscribe-2bee1.firebaseapp.com",
+  projectId: "mediscribe-2bee1",
+  storageBucket: "mediscribe-2bee1.appspot.com",
+  messagingSenderId: "769199842855",
+  appId: "1:769199842855:web:ef12413e691358dfe4d002"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider()
+
+const GoogleSignIn = () => {
+  signInWithPopup(auth, provider).then((result) => {
+
+  }).catch((error) => {
+    console.log(error)
+  })
+};
+
 
 const TextBox = () => {
   return (
@@ -13,7 +39,7 @@ const TextBox = () => {
         Breaking the barrier between doctor and patient is the first step
         towards owning your own health.
       </p>
-      <div className="button">
+      <div className="button" onClick={GoogleSignIn}>
         <Link
           to="/MediScribe"
           style={{
