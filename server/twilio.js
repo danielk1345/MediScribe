e8ffabc8e60d16a000e8a13a2813ce250e52b00f
m6keller <kellermatthewe@gmail.com>
2023-01-21 21:54:43 -0800

    cleaned up function

--- server/twilio.js
+++ server/twilio.js
@@ -6,13 +6,10 @@ const app = express();
 
 app.get("/", async (req, res) => {
   try {
-    // const sgMail = require('@sendgrid/mail')
-    console.log("get request");
-    console.log(process.env.TWILIO);
     sgMail.setApiKey(process.env.TWILIO);
 
     const msg = {
-      to: "leo233257@gmail.com", // Change to your recipient
+      to: "matthewelliottkeller@gmail.com", // Change to your recipient
       //   from: "ll2lau@uwaterloo.ca", // Change to your verified sender
       from: "leo233257@gmail.com",
       subject: "Sending with SendGrid is Fun",
@@ -24,9 +21,9 @@ app.get("/", async (req, res) => {
     console.log("sent", sent);
     res.status(200).json({ response: sent });
   } catch (error) {
-    console.log("error", error);
-    console.log("error.,,,,", error?.response?.body?.errors);
-    res.status(400).json({ error: error.message });
+    res
+      .status(400)
+      .json({ error: error?.message, body: error?.response?.body?.errors });
   }
 });
 
